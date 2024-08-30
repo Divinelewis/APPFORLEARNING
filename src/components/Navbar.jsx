a6fd60e4a59731a96648154760dc8636b8cacca9
divinelewis <divinitysoft0@gmail.com>
2024-08-30 12:42:23 +0100

    Worked on the stylings, aadded a Google Font Called Space Grotesk, Touched a few things on the mobile navigation, Changed the Blank Symbol Icon to a Google custom SVG Material menu  icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import MenuIcon from '../assets/menu.svg';
 
 function NavBar() {
 
@@ -11,15 +12,17 @@ function NavBar() {
   }
 
   return (
-    <nav className="navbar-container">
-        <div className="logo">TestApp</div>
-        <button className="menu-icon" onClick={toggleNav}>☰</button>
-        <ul className={`navbar-links ${openNav ? "open" : ""}`}>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/blog'>Blog</Link></li>
-        </ul>
-    </nav>
+    <div className="container">
+      <nav className="navbar-container">
+          <div className="logo">TestApp</div>
+          <button className="menu-icon" onClick={toggleNav}> <img src={MenuIcon} alt="" /> </button>
+          <ul className={`navbar-links ${openNav ? "open" : ""}`}>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/blog'>Blog</Link></li>
+          </ul>
+      </nav>
+    </div>
   )
 }
 
