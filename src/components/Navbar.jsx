a8a33050990b599c38d347ae8f99b9898ebb3d19
divinelewis <divinitysoft0@gmail.com>
2024-08-30 18:33:08 +0100

    worked on the navigations styling still, played a little with the colors, added more navigation menu items to increase it to six nav items, finally like where I am with this

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import MenuIcon from '../assets/menu.svg';
+import AppForLearning from '../assets/AppForLearning.svg';
 
 function NavBar() {
 
@@ -14,11 +15,14 @@ function NavBar() {
   return (
     <div className="container">
       <nav className="navbar-container">
-          <div className="logo">TestApp</div>
+          <div className="logo"><Link to='/'> <img src={AppForLearning} alt="" /></Link></div>
           <button className="menu-icon" onClick={toggleNav}> <img src={MenuIcon} alt="" /> </button>
           <ul className={`navbar-links ${openNav ? "open" : ""}`}>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/about'>About Us</Link></li>
+            <li><Link to='/#'>FAQ</Link></li>
+            <li><Link to='/#'>Course Content</Link></li>
+            <li><Link to='/#'>Partners</Link></li>
+            <li><Link to='/#'>Challenge</Link></li>
             <li><Link to='/blog'>Blog</Link></li>
           </ul>
       </nav>
