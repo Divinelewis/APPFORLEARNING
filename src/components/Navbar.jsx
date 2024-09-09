b1e9191680fe70c339e16a705cf888b9aefe0ae0
divinelewis <divinitysoft0@gmail.com>
2024-09-09 13:42:05 +0100

    Worked on having some css presets, I noticed that the about page button actually works, but when clicked from another page, it tends to be idle (not performing any task), So i am currently working on resolving this, I have succeeded in getting the page to move from any page it is to the home page, i just need to write a program to automatically scroll the page down to the about section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import './Navbar.css'
 import MenuIcon from '../assets/menu.svg';
 import AppForLearning from '../assets/AppForLearning.svg';
 
 function NavBar({ aboutSectionRef }) {
 
+  const navigate = useNavigate();   // For navigation
+  const location = useLocation();   // To check the current page
+
   const scrollToAbout = () => {
-    if (aboutSectionRef.current) {
+    // If already on the home page, scroll to the about section
+    if (location.pathname === '/') {
       aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      // If not on the home page, first navigate to the home page and then scroll
+      navigate('/');
+      setTimeout(() => {
+        aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+      }, 100); // Delay to allow time for navigation before scrolling
     }
-  }
+  };
 
   const [openNav, setOpenNav] = useState(false);
 
